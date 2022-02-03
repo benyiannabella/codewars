6ab3c3b4ab8ef14bb90d2ced2901502f4a9a4067
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2022-02-03 09:58:54 +0200

    SumOfDifferences

--- src/SumOfDifferences.java
+++ src/SumOfDifferences.java
@@ -5,15 +5,19 @@ import static java.lang.Math.abs;
 
 public class SumOfDifferences {
     public static int sumOfDifferences(int[] arr) {
-        int[] a = Arrays.stream(arr)
-                .boxed()
-                .sorted((x, y) -> y - x)
-                .mapToInt(x -> x)
-                .toArray();
 
-        return IntStream.range(0, a.length-1)
-                .map(i-> a[i] - a[i+1])
-                .sum();
+        return arr.length > 1
+                ? Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt(): 0;
+
+//        int[] a = Arrays.stream(arr)
+//                .boxed()
+//                .sorted((x, y) -> y - x)
+//                .mapToInt(x -> x)
+//                .toArray();
+//
+//        return IntStream.range(0, a.length-1)
+//                .map(i-> a[i] - a[i+1])
+//                .sum();
 
 //        int sum = 0;
 //        if (arr.length <= 1) return 0;
@@ -23,11 +27,10 @@ public class SumOfDifferences {
 //            }
 //        return abs(sum);
 
-
     }
 
     public static void main(String[] args) {
-        System.out.println(sumOfDifferences(new int[]{-17,17}));
+        System.out.println(sumOfDifferences(new int[]{6,4,7}));
     }
 }
 
